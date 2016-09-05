da1e57c2ffc4ed93a7a683b50b2da2e2d55e754a
Wim Selles <w.selles@rn.rabobank.nl>
2016-09-05 12:57:04 +0200

    fix(cucumberjs): fix failure regex to also make it work with single

--- src/parsers/cucumber.multi.js
+++ src/parsers/cucumber.multi.js
@@ -4,7 +4,7 @@ export default {
     let match = null
     let failedSpecs = []
     let testsOutput = output.split('------------------------------------')
-    let RESULT_FAIL = /(.*?) Failures:.*/g
+    let RESULT_FAIL = /Failures:.*/g
     let SPECFILE_REG = /Specs:\s(.*\.feature)/g
     testsOutput.forEach(function (test) {
       let specfile
@@ -12,6 +12,7 @@ export default {
       // only check specs when RESULT_FAIL, ` Specs: ` is always printed when at least multiple features on 1 instance
       // are run with `shardTestFiles: true`
       if (RESULT_FAIL.exec(test)) { // eslint-disable-line no-cond-assign
+        console.log('Failed')
         while (match = SPECFILE_REG.exec(test)) { // eslint-disable-line no-cond-assign
           specfile = match[1]
           result = 'failed'
